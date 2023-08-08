9b2826d4eb3b323888e16e1b2cb004d8b2a9d6c8
paulivallone <pauli.vallone@gmail.com>
2023-08-08 18:01:11 -0300

    entregafinal

--- js/carrito.js
+++ js/carrito.js
@@ -1,4 +1,4 @@
-let obrasEnCarrito = localStorage.getItem("obras-en-carrito");
+let obrasEnCarrito = localStorage.getItem("carrito");
 obrasEnCarrito = JSON.parse(obrasEnCarrito);
 
 const contenedorCarritoVacio = document.querySelector("#carrito-vacio");
@@ -25,22 +25,24 @@ function cargarObrasCarrito() {
       div.innerHTML = `
                 
                 <div class="carrito-obra-titulo">
-                    <small>Título</small>
-                    <h3>${obra.titulo}</h3>
+                    <small>Obra:</small>
+                    <h2>${obra.nombre}</h2>
                 </div>
                 <div class="carrito-obra-cantidad">
-                    <small>Cantidad</small>
+                    <small>Cantidad:</small>
                     <p>${obra.cantidad}</p>
                 </div>
                 <div class="carrito-obra-precio">
-                    <small>Precio</small>
-                    <p>$${obra.precio}</p>
+                    <small>Precio:</small>
+                    <p>$ ${obra.precio}</p>
                 </div>
                 <div class="carrito-obra-subtotal">
-                    <small>Subtotal</small>
-                    <p>$${obra.precio * obra.cantidad}</p>
+                    <small>Subtotal:</small>
+                    <p>$ ${obra.precio * obra.cantidad}</p>
                 </div>
-                <button class="carrito-obra-eliminar" id="${obra.id}"></button>
+                <button class="carrito-obra-eliminar" data-id="${
+                  obra.id
+                }">Eliminar del carrito</button>
             `;
 
       contenedorCarritoObras.append(div);
@@ -60,8 +62,61 @@ cargarObrasCarrito();
 
 function actualizarBotonesEliminar() {
   botonesEliminar = document.querySelectorAll(".carrito-obra-eliminar");
-
   botonesEliminar.forEach((boton) => {
-    boton.addEventListener("click", eliminarDelCarrito);
+    boton.addEventListener("click", () => eliminarDelCarrito(boton.dataset.id));
   });
+}
+
+function eliminarDelCarrito(id) {
+  obrasEnCarrito = obrasEnCarrito.filter((obra) => obra.id != parseInt(id));
+
+  localStorage.setItem("carrito", JSON.stringify(obrasEnCarrito));
+
+  cargarObrasCarrito();
+  actualizarTotal();
+}
+
+botonesEliminar.forEach((boton) => {
+  boton.addEventListener("click", () => eliminarDelCarrito(boton.id));
+});
+
+function actualizarTotal() {
+  let total = 0;
+
+  for (const obra of obrasEnCarrito) {
+    const subtotal = obra.precio * obra.cantidad;
+    total += subtotal;
+  }
+
+  contenedorTotal.textContent = `$${total.toFixed(2)}`;
+}
+
+botonVaciar.addEventListener("click", vaciarCarrito);
+
+function vaciarCarrito() {
+  obrasEnCarrito = [];
+  localStorage.setItem("carrito", JSON.stringify(obrasEnCarrito));
+  cargarObrasCarrito();
+  actualizarTotal();
+}
+
+botonComprar.addEventListener("click", comprarAhora);
+
+function comprarAhora() {
+  vaciarCarrito();
+  mensajeCompraExitosa();
+}
+
+function mensajeCompraExitosa() {
+  Toastify({
+    text: "Muchas gracias por su compra. ¡Nos vemos en la función!",
+    duration: 4000,
+    close: true,
+    gravity: "top",
+    position: "right",
+    stopOnFocus: true,
+    style: {
+      background: "#b94a93",
+    },
+  }).showToast();
 }
--- js/cartelera.js
+++ js/cartelera.js
@@ -1,4 +1,11 @@
-import { obras } from "./obras.js";
+const producto = (a, b) => a * b;
+const costo = (x) => x * 1.1;
+const getObras = async () => {
+  const respuesta = await fetch("/data/data.json");
+  return await respuesta.json();
+};
+
+const obras = await getObras();
 
 const listadoCartelera = document.getElementById("listadoCartelera");
 
@@ -14,9 +21,8 @@ for (const obra of obras) {
     <h3>Nombre: ${obra.nombre}<h3>
     <h3>Genero: ${obra.genero}<h3>
     <p>Precio: $ ${obra.precio}<p>
-    <p>Butacas disponibles: ${obra.stock}<p>
-    <div class='card-footer'>
-    <button  class='btn w-100 agregar-al-carrito' data-obra="${obra.nombre}">Agregar al carrito</button>
+        <div class='card-footer'>
+    <button  class='btn w-100 agregar-al-carrito' data-obra="${obra.nombre}" data-precio="${obra.precio}">Agregar al carrito</button>
     </div>
     </div>
 `;
@@ -37,7 +43,13 @@ function agregarAlCarrito(obra) {
   if (obraEnCarrito) {
     obraEnCarrito.cantidad++;
   } else {
-    carrito.push({ nombre: obra.nombre, cantidad: 1 });
+    let cantidadEntrada = Number(prompt("Ingrese cantidad de entradas"));
+
+    let costoTotal = producto(cantidadEntrada, costo(obra.precio));
+
+    obra.cantidad = cantidadEntrada;
+
+    carrito.push(obra);
   }
 
   localStorage.setItem("carrito", JSON.stringify(carrito));
@@ -46,46 +58,24 @@ function agregarAlCarrito(obra) {
 document.addEventListener("click", function (event) {
   if (event.target.classList.contains("agregar-al-carrito")) {
     const obraNombre = event.target.getAttribute("data-obra");
+    const obraPrecio = parseFloat(event.target.getAttribute("data-precio"));
+
     const obra = obras.find((obra) => obra.nombre === obraNombre);
 
     if (obra) {
       agregarAlCarrito(obra);
-      alert(`Se agregó "${obra.nombre}" al carrito`);
+      Toastify({
+        text: `Se agregó "${obra.nombre}" al carrito. Toca para ir al carrito`,
+        duration: 4000,
+        destination: "../pages/carrito.html",
+        close: true,
+        gravity: "top",
+        position: "right",
+        stopOnFocus: true,
+        style: {
+          background: "#b94a93",
+        },
+      }).showToast();
     }
   }
 });
-
-const producto = (a, b) => a * b;
-const costo = (x) => x * 1.1;
-let costoEntrada = 3000;
-let cantidadEntrada = Number(prompt("Ingrese cantidad de entradas"));
-let costoTotal = producto(cantidadEntrada, costo(costoEntrada));
-console.log("El costo total es de $" + Math.round(costoTotal));
-
-class Obra {
-  constructor(nombre, precio, genero) {
-    this.nombre = nombre.toUpperCase();
-    this.precio = parseFloat(precio);
-    this.genero = genero.toUpperCase();
-  }
-
-  sumarCosto() {
-    this.precio = this.precio * 1.1;
-  }
-}
-/*
-const obras = [];
-obras.push(new Obra("la desgracia", 3500, "comedia"));
-obras.push(new Obra("bajo terapia", 2500, "comedia"));
-obras.push(new Obra("¿estas ahi?", 2500, "drama"));
-
-console.log(obras);
-
-let resultado = obras.filter((el) => el.genero == "COMEDIA");
-
-console.log(resultado);
-
-obras.forEach((obra) => {
-  obra.sumarCosto();
-});
-*/
